e8ff1546e72a414ef72c47e994018903d5f26a09
Dan <dan_merlea@yahoo.com>
2023-08-16 15:43:00 +0300

    Adding constants for networking

---
+++ Foursquare Venues/Application/Constants.swift
@@ -0,0 +1,22 @@
+//
+//  Constants.swift
+//  Foursquare Venues
+//
+//  Created by Dan Merlea on 16.08.2023.
+//
+
+/**
+ Used API v2 with some keys found on Github, could not create a developer account - bug on their side
+ https://location.foursquare.com/developer/reference/v2-venues-search
+ */
+
+struct Constants {
+    
+    struct Foursquare {
+        static let api = "api.foursquare.com"
+        static let clientId = "LH0D1IOIC1SLVTFMNAIVSHHGNF4HKR2K2QSC1NO3MR24H4HV"
+        static let clientSecret = "1PTYZTOJVPD1ZXMS01CUPYRJCQBTRS50CZUARKRS41TRBEN5"
+        static let version = "20191001"
+        static let pageSize = 50
+    }
+}
